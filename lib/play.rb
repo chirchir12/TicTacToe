7c9d7cbd8346f24face0e1cb08afbc53008ce7a7
teshager21 <teshager8922@gmail.com>
2020-06-02 05:57:07 -0600

    add check input validity method

--- lib/play.rb
+++ lib/play.rb
@@ -2,4 +2,14 @@ module Play
   def read_input
     @player_move = gets.chomp
   end
+
+  def check_input_validity
+    if player_move == ''
+      'no input'
+    elsif !%w[1 2 3 4 5 6 7 8 9 e E r R].include?(player_move)
+      'wrong input'
+    else
+      'good'
+    end
+  end
 end
