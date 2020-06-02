9bcd7db5d7408729e12530924d69a661146493ee
teshager21 <teshager8922@gmail.com>
2020-06-02 01:31:17 -0600

    add the intiliazer method

--- lib/board.rb
+++ lib/board.rb
@@ -3,4 +3,13 @@ require_relative 'ui.rb'
 class Board
   include UserInterface
   attr_accessor :cells, :option, :player_name, :state, :message
+
+  def initialize
+    @cells = { '1' => [' ', '1'], '2' => [' ', '2'], '3' => [' ', '3'], '4' => [' ', '4'],
+               '5' => [' ', '5'], '6' => [' ', '6'], '7' => [' ', '7'], '8' => [' ', '8'], '9' => [' ', '9'] }
+    @option = 'Press Q to quit, R to restart'
+    @player_name = 'PLAYER-1'
+    @message = 'WELCOME! PLEASE CHOOSE ONE OF THE NINE CELLS'
+    @state = 'ON'
+  end
 end
