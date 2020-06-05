631a5592b26bf4d1a0c4d1523b8e9ee35be38a85
teshager21 <teshager8922@gmail.com>
2020-06-05 11:42:55 -0600

    test the position taken error

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -22,4 +22,15 @@ describe Game do
       expect(game.board.message).to eql("\n   Wrong Input , Please choose form 1-9")
     end
   end
+
+  describe '#position_taken_error' do
+    it 'changes the message to taken input when player chooses a taken cell' do
+      game.board.cells = { '1' => %w[X 1], '2' => %w[O 2], '3' => %w[X 3], '4' => %w[O 4],
+                           '5' => %w[X 5], '6' => %w[O 6], '7' => %w[O 7], '8' => %w[O 8], '9' => %w[X 9] }
+      game.board.player_move = '1'
+      game.board.check_input_availability
+      game.process_input
+      expect(game.board.message).to eql("\n       That position is taken,\n" + '    choose form the available ones')
+    end
+  end
 end
